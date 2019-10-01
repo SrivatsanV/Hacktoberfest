2885dd4072bee38e5d7a7f3ef0ae39d57b30cbc9
Srivatsan V <srivi567@gmail.com>
2019-10-01 12:19:46 +0530

    Added hello world program written in Rust

---
+++ HelloWorld.rs
@@ -0,0 +1,13 @@
+fn main(){
+    println!("hello world");
+}
+
+/*
+commands to run the program - on linux or MacOS:
+$ rustc HelloWorld.rs
+$ ./main
+
+on Windows:
+rustc HelloWorld.rs
+$ .\main.exe
+*/
